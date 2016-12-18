95f8570dde74f47d7d7f7fcfe482d8f38772217e
rafaelaudy <rafael.audy@gmail.com>
2016-12-18 23:37:28 +0000

    Removing inline styles

--- src/components/TripCard.jsx
+++ src/components/TripCard.jsx
@@ -1,21 +1,21 @@
 import React from 'react';
 
+import './tripCard.scss';
+
 export default (props) => {
     return (
-		<div style={{ padding: "10px"}}>
-			<div className="demo-card-square mdl-card mdl-shadow--4dp">
-				<div className="mdl-card__title mdl-card--expand">
-					<h2 className="mdl-card__title-text">Update</h2>
-				</div>
-				<div className="mdl-card__supporting-text">
-					Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-					Aenan convallis.
-				</div>
-				<div className="mdl-card__actions mdl-card--border">
-					<a className="mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect">
-					View Updates
-					</a>
-				</div>
+		<div className="trip-card-square mdl-card mdl-shadow--4dp">
+			<div className="mdl-card__title mdl-card--expand">
+				<h2 className="mdl-card__title-text">Update</h2>
+			</div>
+			<div className="mdl-card__supporting-text">
+				Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+				Aenan convallis.
+			</div>
+			<div className="mdl-card__actions mdl-card--border">
+				<a className="mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect">
+				View Updates
+				</a>
 			</div>
 		</div>
     );
