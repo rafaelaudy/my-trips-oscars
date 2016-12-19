b164b81941392d117f3b383308cff20fd9d9f4b7
rafaelaudy <rafael.audy@gmail.com>
2016-12-19 23:57:07 +0000

    Adding a more meaningfull card

--- src/js/components/TripCard.jsx
+++ src/js/components/TripCard.jsx
@@ -6,15 +6,14 @@ export default (props) => {
     return (
 		<div className="trip-card-square mdl-card mdl-shadow--4dp">
 			<div className="mdl-card__title mdl-card--expand">
-				<h2 className="mdl-card__title-text">Update</h2>
+				<h2 className="mdl-card__title-text">Veneza</h2>
 			</div>
 			<div className="mdl-card__supporting-text">
-				Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-				Aenan convallis.
+				Gondolas, pombas, multidāo e pasta (rica pasta).
 			</div>
 			<div className="mdl-card__actions mdl-card--border">
 				<a className="mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect">
-				View Updates
+				Ver oscars!
 				</a>
 			</div>
 		</div>
