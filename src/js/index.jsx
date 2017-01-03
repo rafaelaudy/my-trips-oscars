2a5c41d7f67e830758427b7b2d6c1d287f59249b
rafaelaudy <rafael.audy@gmail.com>
2017-01-03 21:13:25 +0000

    Adding redux to the project

--- src/js/index.jsx
+++ src/js/index.jsx
@@ -1,5 +1,8 @@
 import * as React from 'react';
 import { render } from 'react-dom';
+import { Provider } from 'react-redux';
+
+import store from './store/store';
 
 import App from './components/App';
 
